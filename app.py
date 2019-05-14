063574d267f99e7d7fa153b6fdf042afdc14a6b8
FerreWagner <18408229270@163.com>
2019-05-14 21:41:39 +0800

    add flask route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request,url_for
 
 app = Flask(__name__)
 
@@ -6,6 +6,24 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello j!'
+
+@app.route('/flower', methods=['GET'])
+def faceless():
+    return 'faceless folwer'
+
+@app.route('/buy/<num>')
+def buy(num):
+    return num
+
+@app.route('/query')
+def query():
+    id = request.args.get('id')
+    return 'query: ' + id
+
+# 反向路由
+@app.route('/query_url')
+def query_url():
+    return 'query_url: ' + url_for('query')
 
 
 if __name__ == '__main__':
