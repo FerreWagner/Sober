13f1a1ffe5f8471ac71fdf99be9ea818029e8b3b
FerreWagner <18408229270@163.com>
2019-05-14 22:50:32 +0800

    update template

--- flaskapp.py
+++ flaskapp.py
@@ -19,8 +19,27 @@ def user_index():
 def query_user(user_id):
     user = None
     if int(user_id) == 1:
-        user = User(1, 'J user')
-    return render_template("user_id.html", user=user)
+        user = User(1, 'J-user')
+    return render_template("user_id.html", user = user)
+
+@app.route('/users')
+def user_list():
+    users = []
+    for i in range(1, 11):
+        user = User(1, 'J-user' + str(i))
+        users.append(user)
+    return render_template("user_list.html", users = users)
+
+@app.route('/one')
+def one_base():
+    return render_template("one_base.html")
+
+@app.route('/two')
+def two_base():
+    return render_template("two_base.html")
+
+
+
 
 
 if __name__ == '__main__':
