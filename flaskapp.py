1ae94b8925a45afdfb9f5c281fe384b901541677
FerreWagner <18408229270@163.com>
2019-05-14 22:37:19 +0800

    update error judge

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,6 @@
 from flask import Flask,request,url_for,render_template
 from models import User
+import sys
 
 app = Flask(__name__)
 
@@ -13,6 +14,14 @@ def hello_world():
 def user_index():
     users = User(1, 'For Flower')
     return render_template("user_index.html", user = users)
+
+@app.route('/query_user/<user_id>')
+def query_user(user_id):
+    user = None
+    if int(user_id) == 1:
+        user = User(1, 'J user')
+    return render_template("user_id.html", user=user)
+
 
 if __name__ == '__main__':
     app.run()
